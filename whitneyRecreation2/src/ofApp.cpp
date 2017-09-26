e26844f79153cd7db370c7e9a95fb69bb19219f4
Ann Kidder <annkidder@Anns-Air-2.fios-router.home>
2017-09-26 16:23:33 -0400

    added third wave

--- whitneyRecreation2/src/ofApp.cpp
+++ whitneyRecreation2/src/ofApp.cpp
@@ -24,9 +24,16 @@ void ofApp::draw(){
     
     float x_2, y_2;
     for (int i=0;i<200;i++) {
-        x_2 = (float)-100.0 + i*time*5;
+        x_2 = (float) + i*time*5;
         y_2 = 350.0 + 250 * sin(i*.05-PI/2);
         ofDrawCircle(x_2, y_2, 5);
+    }
+    
+    float x_3, y_3;
+    for (int i=0;i<200;i++) {
+        x_3 = (float)-ofGetWidth()/2.0 + i*time*5;
+        y_3 = 400.0 + 250 * sin(i*.05-PI/2);
+        ofDrawCircle(x_3, y_3, 5);
     }
 }
 
